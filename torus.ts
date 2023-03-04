5e4d94757b8326abfa2cb8c4b2309f060addc3a8
anwolosz <anwolosz@gmail.com>
2023-03-04 22:05:34 +0100

    toArray(), indexToCoordinates()

--- torus.ts
+++ torus.ts
@@ -62,6 +62,42 @@ class Torus {
         this.torus[this.coordinatesToIndex(coordinates)] = value
     }
 
+    toArray() {
+        return this.buildArray(this.torus, this.dimensions[0], this._dimensions.slice(1))
+    }
+
+    private buildArray(values: any[], dimension: number, dimensions: number[]) {
+        let currentArray = new Array(dimension)
+        for (let i = 0; i < dimension; i++) {
+            if (dimensions.length == 0) {
+                currentArray[i] = values[i]
+            }
+            else {
+                const subValuesLength = values.length / dimension
+                currentArray[i] = this.buildArray(
+                    values.slice(i * subValuesLength, (i + 1) * subValuesLength),
+                    dimensions[0],
+                    dimensions.slice(1)
+                )
+            }
+        }
+        return currentArray
+    }
+
+    private indexToCoordinates(index: number) {
+        let coordinates: number[] = []
+        for (let i = 0; i < this._dimension; i++) {
+            let dimensionsProduct = 1
+            for (let j = 1 + i; j < this._dimension; j++) {
+                dimensionsProduct *= this._dimensions[j]
+            }
+            const quotient = Math.floor(index / dimensionsProduct)
+            coordinates.push(quotient)
+            index -= quotient * dimensionsProduct
+        }
+        return coordinates
+    }
+
     public get dimension() {
         return this._dimension
     }
